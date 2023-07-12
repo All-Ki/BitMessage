c41251f2483b7c88236fe144bb154d6771854c19
Ki <k.allegrain@gmail.com>
2023-07-12 11:36:47 +0200

    updated stuff

--- webapp/backend/src/index.ts
+++ webapp/backend/src/index.ts
@@ -1,7 +1,8 @@
+import { log } from 'console';
 import cors from 'cors'
 import express from 'express'
-
-
+import {User, Message} from './models'
+import { Sequelize } from 'sequelize';
 const PORT = 5656
 /**
  * On créé une nouvelle "application" express
@@ -26,6 +27,38 @@ app.use(cors())
 app.get('/', (req, res) => res.send('ma bite'))
 
 
+var sampleMessage = {
+	  id: "1",
+	  text: "Hello",
+	  date: "2019-01-01",
+	  sender: "1",
+	  receiver: "2",
+}
+
+
+type NewMessage = {
+	text: string,
+	sender: number,
+	receiver: number,
+}
+app.post('/messages', (req, res) => {
+	console.log(req.body);
+	const message : NewMessage = req.body;
+	const builtMessage = Message.build(message);
+	builtMessage.save();
+	log(message);
+	res.send(message);
+})
+
+app.get('/messages/:receiver', async (req, res) => {
+	const receiver = req.params.receiver;
+	const msgs = await Message.findAll({
+		where: {
+			receiver: receiver,
+		}
+	})
+	res.send(msgs);
+})
 /**
  * On demande à Express d'ecouter les requêtes sur le port défini dans la config
  */
---
+++ webapp/backend/src/models.ts
@@ -0,0 +1,39 @@
+import { Sequelize, DataTypes } from 'sequelize';
+const sequelize = new Sequelize('sqlite::memory:');
+
+
+export const Message = sequelize.define('Message', {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	text: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	date: {
+		type: DataTypes.DATE,
+		allowNull: false,
+	},
+	sender: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+	receiver: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+	},
+})
+
+export const User = sequelize.define('User', {
+	id: {
+		type: DataTypes.INTEGER,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	username: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+})
