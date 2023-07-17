344f5eac71a20390113814fd8d5ba812e0da424b
Ki <k.allegrain@gmail.com>
2023-07-17 18:27:37 +0200

    added api wrapper

---
+++ webapp/frontend/src/app/api/api/api.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import axios, { AxiosInstance, AxiosPromise, AxiosRequestConfig } from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private static axiosInstance: AxiosInstance = axios.create({
+    baseURL: environment.url,
+    timeout: 1000
+  });
+  private axiosInstance: AxiosInstance;
+
+
+  constructor() {
+    this.axiosInstance = ApiService.axiosInstance;
+  }
+
+  // Static wrapper method to forward GET requests
+  static get<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return ApiService.axiosInstance.get(url, config);
+  }
+
+  // Static wrapper method to forward POST requests
+  static post<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return ApiService.axiosInstance.post(url, data, config);
+  }
+
+
+
+  // Wrapper method to forward GET requests
+  public get<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.get(url, config);
+  }
+
+  // Wrapper method to forward POST requests
+  public post<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.post(url, data, config);
+  }
+
+  // Wrapper method to forward PUT requests
+  public put<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.put(url, data, config);
+  }
+
+  // Wrapper method to forward PATCH requests
+  public patch<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.patch(url, data, config);
+  }
+
+  // Wrapper method to forward DELETE requests
+  public delete<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.delete(url, config);
+  }
+
+  // Wrapper method to forward HEAD requests
+  public head<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.head(url, config);
+  }
+
+  // Wrapper method to forward OPTIONS requests
+  public options<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T> {
+    return this.axiosInstance.options(url, config);
+  }
+
+  // Add other wrapper methods for additional HTTP methods as needed
+}
--- webapp/frontend/src/app/api/users/users.service.ts
+++ webapp/frontend/src/app/api/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import axios from 'axios';
 import * as Accounts from 'web3-eth-accounts';
+import { ApiService } from '../api/api.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,14 +17,20 @@ export class UsersService {
   private private_key: string = "";
   private wallet: any;
   private accounts: any;
+
   async login(private_key: string) : Promise<boolean> {
     this.private_key = private_key;
 
     let acc = Accounts.privateKeyToAccount(private_key);
     let signed = acc.sign('Login from ' + acc.address);
+    /*
+    console.log( "signed message : ")
+    console.log(signed)
+    console.log("account : ")
     console.log(acc)
+  */
     console.log("recovered : " + Accounts.recover('Login from ' + acc.address , signed.signature))
-    const res = await axios.post(environment.url + '/login',
+    const res = await ApiService.post('/login',
     {public_key:acc.address, encrypted_message:signed});
     this.wallet = acc;
     console.log('login ' + private_key);
