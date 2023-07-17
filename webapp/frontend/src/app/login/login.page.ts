df59084389e5c730cad7459a9c9ec5bcc8106dc4
Alex <alexandre.eymoz@gmail.com>
2023-07-17 16:08:29 +0200

    Login v2

--- webapp/frontend/src/app/login/login.page.ts
+++ webapp/frontend/src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { UsersService } from '../api/users/users.service';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +9,12 @@ import { Router } from "@angular/router";
 })
 export class LoginPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private userService: UsersService) { }
 
   ngOnInit() {
   }
 
+  login(form: any){
+    this.userService.login
+  }
 }
