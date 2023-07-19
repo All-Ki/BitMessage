09c7fb75965a2086c3f98e097834a175ce477195
Alex <alexandre.eymoz@gmail.com>
2023-07-19 14:09:02 +0200

    Add loading screen to loginPage + redirect from login to messages list if the user enter the right key + displays an error message if the user enters the wrong key

--- webapp/frontend/src/app/login/login.page.ts
+++ webapp/frontend/src/app/login/login.page.ts
@@ -18,14 +18,20 @@ export class LoginPage implements OnInit {
   }
 
   public private_key: string = "0x8da4ef21b864d2cc526dbdb2a120bd2874c36c9d0a1fb7f8c63d7f7a8b41de8f";
+  public errorMessage: string = "";
+  public loading: boolean = false;
   async login(){
-     const loginSuccess = await this.userService.login(this.private_key);
+    this.loading = true;
+    
+    const loginSuccess = await this.userService.login(this.private_key);
 
-     if (loginSuccess) {
+    this.loading = false;
+
+    if (loginSuccess) {
       this.navCtrl.navigateForward([CONSTANTS.discussion_list_page]);
-     } else {
-      console.log("Fuck You");
-     }
+    } else {
+      this.errorMessage = "Invalid Key";
+    }
   }
 
 }
