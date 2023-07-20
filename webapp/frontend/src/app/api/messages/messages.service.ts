721a4b69486d35d2d11b875967363cbaeca99eae
Ki <k.allegrain@gmail.com>
2023-07-20 10:32:23 +0200

    changed message service data structures; removed padding bottom on ion list

--- webapp/frontend/src/app/api/messages/messages.service.ts
+++ webapp/frontend/src/app/api/messages/messages.service.ts
@@ -6,6 +6,7 @@ import { ServiceWithInit } from 'src/app/services/service-with-init';
 import { StorageService } from 'src/app/services/storage.service';
 
 export class Message {
+  id?: number;
   text: string = '' ;
   sender: string = "" ;
   receiver: string = "";
@@ -16,13 +17,20 @@ export class Message {
   providedIn: 'root'
 })
 export class MessagesService extends ServiceWithInit{
+  Discussions: Map<string, Message[]> = new Map<string, Message[]>();
+  current_user = '';
 
   constructor(private userSvc: UsersService, private storage: StorageService) {
     super(storage);
   }
+  override async OnStorageReady() {
+    this.current_user = this.userSvc.getCurrentUser();
+    this.Discussions = await this.storage.get('discussions') || new Map<string, Message[]>();
 
-  Messages: Message[] = [];
-  Discussions: Map<string, Message[]> = new Map<string, Message[]>();
+  }
+  async saveMessages(){
+    await this.storage.set('discussions', this.Discussions);
+  }
   async postMessage(text: string, receiver: string) {
     const sender = this.userSvc.getCurrentUser();
     const msg = {
@@ -32,20 +40,36 @@ export class MessagesService extends ServiceWithInit{
       date: new Date(),
     }
     await axios.post(environment.url + '/messages', msg);
-    this.Discussions.get(receiver.toString())?.push(msg);
-
+    let messages = this.Discussions.get(receiver);
+    if (messages == null) {
+      messages = [];
+    }
+    messages.push(msg);
+    this.Discussions.set(receiver, messages);
+    await this.saveMessages();
   }
 
   async getMessagesFrom(from : string) : Promise<any>{
     const receiver = this.userSvc.getCurrentUser();
     const res = await axios.get(environment.url + '/messages/' + receiver + '/' + from);
-    return res.data;
+    let msgs = res.data;
+    let messages = this.Discussions.get(from) || [];
+
+    msgs.forEach((msg: any )=> {
+      if(messages.find(m => m.id == msg.id) == null){
+        messages.push(msg);
+      }
+    });
+    messages.sort((a: Message, b: Message) => {
+      return a.date.getTime() - b.date.getTime();
+    });
+    this.Discussions.set(from, messages);
+    await this.saveMessages();
+
+    return messages;
   }
   async getDiscussions() : Promise<any>{
-    const user = this.userSvc.getCurrentUser();
-    console.log('getDiscussions');
-    console.log(user);
-    const res = await axios.get(environment.url + '/discussions/' + user);
+    const res = await axios.get(environment.url + '/discussions/' + this.current_user);
     return res.data;
   }
 }
