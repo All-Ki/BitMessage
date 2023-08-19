a4812af136e57fea62be3bfda73ce68232ebfc77
Ki <k.allegrain@gmail.com>
2023-08-19 15:13:25 +0200

    added solana smart contract

--- blockchain/solana/src/lib.rs
+++ blockchain/solana/src/lib.rs
@@ -1,16 +1,74 @@
 use solana_program::{
-    account_info::AccountInfo,
+    account_info::{next_account_info, AccountInfo},
     entrypoint,
     entrypoint::ProgramResult,
-    pubkey::Pubkey,
     msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
 };
+
+// Define the data structure for the RSA key
+pub struct RSAKey {
+    key: [u8; 256], // Replace with the appropriate key size
+}
+
+// Entry point function
 entrypoint!(process_instruction);
-pub fn process_instruction(
+
+// Main processing function
+fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8]
+    _instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("Hello, world!");
+    // Get the accounts involved
+    let accounts_iter = &mut accounts.iter();
+    let wallet_account = next_account_info(accounts_iter)?;
+    let owner_account = next_account_info(accounts_iter)?;
+    let system_program_account = next_account_info(accounts_iter)?;
+
+    // Check if the owner is the signer of the transaction
+    if !owner_account.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+
+    // Deserialize the RSA key from the wallet account's data
+    let mut rsa_key_data = wallet_account.data.borrow_mut();
+    let rsa_key = RSAKey::deserialize(&mut rsa_key_data)?;
+
+    // Implement your RSA key verification logic here
+    // ...
+
+    // Implement the fee logic here
+    // ...
+
+    // Update the RSA key in the account's data
+    rsa_key.serialize(&mut rsa_key_data)?;
+
     Ok(())
+}
+
+impl RSAKey {
+    // Deserialize an RSA key from byte data
+    fn deserialize(data: &mut &[u8]) -> Result<Self, ProgramError> {
+        // Implement deserialization logic here
+        // ...
+
+        // Example deserialization (256 bytes key size)
+        let mut key = [0u8; 256];
+        data.read_exact(&mut key)?;
+
+        Ok(RSAKey { key })
+    }
+
+    // Serialize an RSA key into byte data
+    fn serialize(&self, data: &mut &mut [u8]) -> Result<(), ProgramError> {
+        // Implement serialization logic here
+        // ...
+
+        // Example serialization (256 bytes key size)
+        data.write_all(&self.key)?;
+
+        Ok(())
+    }
 }
